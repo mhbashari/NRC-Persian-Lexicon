c05e2f09648ab8a2c61753d5b325f07b064039e2
mhbashari <mhbashari@gmail.com>
2016-03-17 15:06:00 -0700

    -Readme

--- NRCPersianLexiconMatch.py
+++ NRCPersianLexiconMatch.py
@@ -1,1 +1,15 @@
-__author__ = 'Administrator'
+__author__ = 'mhbashari'
+
+import pandas as pd
+
+
+class ExcelReader:
+    def __init__(self, excel_address="NRC-Emotion-Lexicon-v0.92-InManyLanguages-web.xlsx"):
+        self.excel_address = excel_address
+        self.data = {}
+
+    def load(self):
+        xl = pd.ExcelFile("dummydata.xlsx")
+
+    def get_translation(self, word):
+        return self.data[word]
