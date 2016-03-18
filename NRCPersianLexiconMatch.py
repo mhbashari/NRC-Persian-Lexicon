da27aaedac6a386695097bf4b1cdfe482dbd3b9a
mhbashari <mhbashari@gmail.com>
2016-03-18 00:04:06 -0700

    Readme/Matcher/Make matched file

--- NRCPersianLexiconMatch.py
+++ NRCPersianLexiconMatch.py
@@ -1,6 +1,6 @@
 __author__ = 'mhbashari'
 
-import pandas as pd
+import xlrd as xrd
 
 
 class ExcelReader:
@@ -8,8 +8,30 @@ class ExcelReader:
         self.excel_address = excel_address
         self.data = {}
 
-    def load(self):
-        xl = pd.ExcelFile("dummydata.xlsx")
+    def load(self, col_idx=22):  # default for persian (in current version)
+        xl_workbook = xrd.open_workbook(self.excel_address)
+        sheet_names = xl_workbook.sheet_names()
+        xl_sheet = xl_workbook.sheet_by_name(sheet_names[0])
+        for row_idx in range(1, xl_sheet.nrows):
+            translation = xl_sheet.cell(row_idx, col_idx)  # Get cell object by row, col
+            english = xl_sheet.cell(row_idx, 0)  # Get cell object by row, col
+            if translation.value is not None and len(translation.value) > 0:
+                self.data[english.value] = translation.value
 
     def get_translation(self, word):
         return self.data[word]
+
+    def has_key(self, key):
+        return key in self.data.keys()
+
+
+if __name__ == "__main__":
+    reader = ExcelReader()
+    reader.load()
+    engligh_text_file = open("NRC-emotion-lexicon-wordlevel-alphabetized-v0.92.txt", "r", encoding="utf-8")
+    destionation_text_file = open("NRC-emotion-lexicon-wordlevel-persian-v0.92.txt", "w", encoding="utf-8")
+    for line in engligh_text_file.readlines():
+        spl = line.split("\t")
+        word, emotion, value = spl[0], spl[1], spl[2]
+        if reader.has_key(word):
+            destionation_text_file.write("\t".join([reader.get_translation(word), emotion, value]))
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 NRC Word-Emotion Association Lexicon 
 The [NRC Emotion Lexicon](http://saifmohammad.com/WebPages/NRC-Emotion-Lexicon.htm) is a list of English words and their associations with eight basic emotions (anger, fear, anticipation, trust, surprise, sadness, joy, and disgust) and two sentiments (negative and positive). The annotations were manually done by crowdsourcing.
 ## Persian Translation of The Lexicon
-There is an Excel file that contains the translation of the english words (without complete coverage on all languages).I wrote a small code to combine the main text file (containing emotion annotations) and Excel translation file.
+There is an Excel file that contains the translation (Google Translate) of the english words (without complete coverage on all languages).I wrote a small code to combine the main text file (containing emotion annotations) and Excel translation file.
 ## Dependency
 1. pandas
 
